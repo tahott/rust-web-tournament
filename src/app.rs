09ffbc8ac5a4785cf2cdeba3b4b2ef995b567255
tahott <tahott@gmail.com>
2022-05-20 00:59:34 +0900

    Add change modal state button

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,46 @@
-use std::rc::Rc;
+use std::{rc::Rc, str::FromStr};
+use gloo::utils::document;
+use serde::Serialize;
+use wasm_bindgen::{JsValue, JsCast};
+use web_sys::{HtmlSelectElement, HtmlInputElement, console};
 use yew::prelude::*;
 
 use crate::component::TournamentModal;
+
+#[derive(Serialize)]
+enum TournamentType {
+  #[serde(rename="tournament")]
+  Tournament,
+  #[serde(rename="swissLeague")]
+  SwissLeague,
+}
+
+impl FromStr for TournamentType {
+  type Err = ();
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "tournament" => Ok(TournamentType::Tournament),
+      "swiss_league" => Ok(TournamentType::SwissLeague),
+      _ => Err(()),
+    }
+  }
+}
+
+#[derive(Serialize)]
+struct TournamentState {
+  #[serde(rename="tournamentType")]
+  tournament_type: TournamentType,
+  participants: u8,
+  title: String,
+}
 
 enum StateAction {
   Open,
   Close,
 }
 struct ModalState {
-  is_open: bool
+  pub is_open: bool
 }
 
 impl Reducible for ModalState {
@@ -40,6 +72,31 @@ pub fn app() -> Html {
     }
   };
 
+  let change_modal_state = {
+    let modal_state_handle = modal_state_handle.clone();
+
+    if modal_state_handle.is_open == false {
+      Callback::from(move |_| {
+        modal_state_handle.dispatch(StateAction::Open);
+      })
+    } else {
+      Callback::from(move |_| {
+        let val = parse_element::<HtmlSelectElement>("tournament_type").value();
+        let participants = parse_element::<HtmlInputElement>("participants").value_as_number();
+        let title = parse_element::<HtmlInputElement>("title").value();
+
+        let tournament = TournamentState {
+          tournament_type: TournamentType::from_str(&val).unwrap(),
+          participants: participants as u8,
+          title,
+        };
+
+        console::log_1(&JsValue::from_serde(&tournament).unwrap());
+        modal_state_handle.dispatch(StateAction::Close);
+      })
+    }
+  };
+
   html!(
     <div class="py-2">
       <div class="container mx-auto h-screen">
@@ -49,9 +106,13 @@ pub fn app() -> Html {
             {"토너먼트 생성"}
           </button>
         </div>
-        <TournamentModal state={modal_state_handle.is_open} />
+        <TournamentModal state={modal_state_handle.is_open} modal_state_handle={change_modal_state} />
         // Grid
       </div>
     </div>
   )
+}
+
+fn parse_element<T: wasm_bindgen::JsCast>(id: &str) -> T {
+  document().get_element_by_id(id).unwrap().dyn_into::<T>().unwrap()
 }
--- src/component/modal.rs
+++ src/component/modal.rs
@@ -1,43 +1,28 @@
-use gloo::{utils::document, console::console};
-use wasm_bindgen::{JsValue, JsCast};
-use web_sys::{console, HtmlSelectElement, HtmlInputElement};
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct ModalProps {
-  pub state: bool
+  pub state: bool,
+  pub modal_state_handle: Callback<MouseEvent>
 }
 
 #[function_component(TournamentModal)]
 pub fn Modal(props: &ModalProps) -> Html {
-  let onclick = {
-    Callback::from(move |_| {
-      let val = parse_element::<HtmlSelectElement>("tournament_type").value();
-      let participants = parse_element::<HtmlInputElement>("participants").value_as_number();
-      let title = parse_element::<HtmlInputElement>("title").value();
-      console::log_3(&JsValue::from_str(&val), &JsValue::from_f64(participants), &JsValue::from_str(&title));
-    })
-  };
-
   html!(
     <div class={if props.state {"max-w-lg w-full absolute mx-auto p-4 text-white rounded-lg shadow-2xl bg-indigo-500 shadow-indigo-500/50"} else {"hidden"}}>
       <div class="grid grid-cols-2 gap-2 mb-6">
         <select id="tournament_type" class="bg-indigo-400">
-          <option value="토너먼트" selected={true}>{"토너먼트"}</option>
-          <option value="스위스 리그" disabled={true}>{"스위스 리그"}</option>
+          <option value="tournament" selected={true}>{"토너먼트"}</option>
+          <option value="swiss_league" disabled={true}>{"스위스 리그"}</option>
         </select>
-        <input id="participants" class="bg-indigo-400 rounded" type="number" min=2 max=64 />
+        <input id="participants" class="bg-indigo-400 rounded pl-2" type="number" min=2 max=64 value=2 />
       </div>
       <div>
         <input id="title" class="rounded w-full pl-2 mb-6 bg-indigo-400" type="text" placeholder="title" />
       </div>
       <div>
-        <button type="button" class="rounded bg-indigo-200 w-full" {onclick}>{"생성"}</button>
+        <button type="button" class="rounded bg-indigo-200 w-full" onclick={props.modal_state_handle.clone()}>{"생성"}</button>
       </div>
     </div>
   )
-}
-
-fn parse_element<T: wasm_bindgen::JsCast>(id: &str) -> T {
-  document().get_element_by_id(id).unwrap().dyn_into::<T>().unwrap()
 }
